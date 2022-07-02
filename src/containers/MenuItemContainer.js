a285af39c97d2a61a24d73ece024f3be3c1bb0e4
Viktor <viktor.m.rumenov@gmail.com>
2022-07-03 00:07:48 +0200

    Added calculation logic for tip and amount using selectors

--- src/containers/MenuItemContainer.js
+++ src/containers/MenuItemContainer.js
@@ -1,18 +1,26 @@
+import { connect } from "react-redux";
 import {
   removeItem,
   updatePrice,
   updateQuantity,
 } from "../store/items/actions";
-import { connect } from "react-redux";
 import MenuItem from "../components/MenuItem";
+import { selectItemTotal } from "../store/items/selectors";
+
+const mapStateToProps = (state, props) => ({
+  total: selectItemTotal(state, props),
+});
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    remove: (uuid) => dispatch(removeItem(ownProps.uuid)),
+    remove: () => dispatch(removeItem(ownProps.uuid)),
     updatePrice: (price) => dispatch(updatePrice(ownProps.uuid, price)),
     updateQuantity: (quantity) =>
       dispatch(updateQuantity(ownProps.uuid, quantity)),
   };
 };
 
-export const MenuItemContainer = connect(null, mapDispatchToProps)(MenuItem);
+export const MenuItemContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MenuItem);
